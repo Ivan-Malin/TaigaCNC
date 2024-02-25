696d013224bb61f7976e834c9fa17ffd1b3ecbd7
LAPTOP-0DFODGAS\Admin <malili2003li@gmail.com>
2024-02-25 20:11:15 +0300

    [Unstable] Added base implementation of cnc side

---
+++ zcnc_server/cnc_processor/PRINTERS/ANYCUBIC.py
@@ -0,0 +1,14 @@
+
+'''
+Write please python script with py simple gui with 3 columns.
+
+1 (left) column contains settings
+
+2 column contains ID (gui boxes/cards), where you can drag and drop cards from 3 column
+
+3 column contains cards with CNC names.
+
+After drag and dropping card from 3 column (i.e. CNC) to card from 2 column (ID) (i.e. put it in card from 2 column), card should disappear from 3 column and appear inside of the card from 2 column. Also app must provide drag and drop feature to drop CNC from ID back to 3 column (i.e. general CNC storage of CNC without ID)
+
+App should store those cards, and also information about which CNC is putted in which ID in settings in local file (.json). Also it should provide feature to add new cards to 2 column and 3 column using script
+'''
